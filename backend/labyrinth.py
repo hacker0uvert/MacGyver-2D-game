665e87da15a3cdb631c65c71c636ae69275b083d
hacker0uvert <52000851+hacker0uvert@users.noreply.github.com>
2020-05-11 18:43:46 +0200

    add backend labyrinth file

---
+++ backend/labyrinth.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+""" Module generating and interacting with the 15 boxes grid labyrinth on which the game is played
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- game.py
+++ game.py
@@ -2,3 +2,9 @@
 # coding: utf-8
 
 import pygame
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
