55c798b81a7e5c86c2e428ad29d3799c714bdf99
hacker0uvert <52000851+hacker0uvert@users.noreply.github.com>
2020-06-22 17:40:47 +0200

    game loop creation in game module
    Signed-off-by: hacker0uvert <52000851+hacker0uvert@users.noreply.github.com>

--- game.py
+++ game.py
@@ -13,6 +13,23 @@ def main():
     display.load()
     surfaces = frtd.surfaces_dict()
     display.background_init(surfaces)
+    clock = frtd.CLOCK
+
+    # game time
+    on_air = True
+    while on_air:
+        # Frames Per Second definition
+        clock.tick(frtd.stg.FPS)
+        # quit event definition
+        for event in frtd.pg.event.get():
+            if event.type == frtd.pg.QUIT:
+                on_air = False
+        frtd.pg.display.update()
+        frtd.SPRITES.draw(display.screen)
+        frtd.pg.display.update()
+
+frtd.pg.quit()
+
 
 if __name__ == '__main__':
     main()
